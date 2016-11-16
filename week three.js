a48b0650031edff33f059a6fca5a56bab379332b
Kevin Siwoff <info@kevinsiwoff.com>
2016-11-15 21:57:57 -0500

    adds comments to week 3

--- week three.js
+++ week three.js
@@ -1,3 +1,8 @@
+/**
+** Kevin's comments: I like the start of this idea.  I'd like to see you push yourself a little further in terms of the design,
+maybe try different colors or dynamic positioning of the ellipses.  Also, the code as-is will not run because you have an undefined
+variable error.  See my inline comments for more details...
+**/
 var theta = 0.0;
 var ellipseWidthMultiplier = 10;
 var ellipseHeightMultiplier = 20;
@@ -14,6 +19,8 @@ function draw() {
   //stationary ellipse
   push();
   translate(width/2, height/2);
+  //since you repeat a for loop like this 4 times, it's a perfect
+  //opportunity to use a function so that you don't have to write out the for loop each time.
   for(var i =0; i < 25; i++){
     ellipse(0, 0,
     i*ellipseWidthMultiplier,
@@ -51,6 +58,7 @@ function draw() {
   rotate(theta);
   for(var i =0; i < 25; i++){
     ellipse(0, 0,
+    // it looks like you have a typo with ellipseWidthMultiplier2.  Perhaps you left off a / operator?
     i*(ellipseWidthMultiplier2),
     i*(ellipseHeightMultiplier/2));
   }
